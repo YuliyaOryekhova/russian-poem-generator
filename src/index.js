d75ca2e2ccd81ba0558e02535846869a1b2ab426
YuliyaOryekhova <yuliavit7@gmail.com>
2024-07-03 15:14:59 -0400

    Added basic form handler and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings: "Я вас любил: любовь еще, быть может",
+    autoStart: true,
+    delay: 150,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
